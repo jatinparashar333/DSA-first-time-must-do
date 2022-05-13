d7713789d4f417d406464ecdc8f2ec643ebc13c1
Jatin Parashar <78733719+ParasharJatin333@users.noreply.github.com>
2022-05-13 14:53:09 +0530

    print all components of a graph

---
+++ GRAPH/print all components of graph.cpp
@@ -0,0 +1,51 @@
+//print all components of graph
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ft first
+#define sc second
+#define pb push_back
+#define ins insert
+#define all(v) v.begin(), v.end()
+#define Sort(v) sort(all(v))
+#define s(a, n) sort(a, a + n)
+#define f1(i,a,b) for(ll i=a;i<b;i++)
+#define f2(i,a,b,c) for(ll i=a;i<b;i+=c)
+#define f3(i,a,b) for(ll i=a;i>=b;i--)
+#define f4(i,a,b,c) for(ll i=a;i>=b;i-=c)
+#define jatin(t) int t; cin >> t; while(t--)
+const int N = 1e5+2, MOD = 1e9+7;
+//int array[N];
+//cout << (condition ? "YES\n" : "NO\n");
+vector<int> adj[N];
+bool vis[N];
+void DFS(int n){
+	cout<<n<<" ";
+	vis[n]=true;
+	vector<int> :: iterator it;
+	for(it=adj[n].begin();it!=adj[n].end();it++){
+		if(!vis[*it]){
+			DFS(*it);
+		}
+	}
+}
+int main(){
+	int n,m;cin>>n>>m;
+	for(int i=0;i<m;i++){
+		int x,y;
+		cin>>x>>y;
+		adj[x].push_back(y);
+		adj[y].push_back(x);
+	}
+	for(int i=1;i<=n;i++){
+		vis[i]=false;
+	}
+	for(int i=1;i<=n;i++){
+		
+		if(!vis[i]){
+			cout<<"connected component : ";
+			DFS(i);
+			cout<<endl;
+		}
+	}
+}
